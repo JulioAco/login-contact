a9fa27e506b6a1f9bad678977a11ee8c8a4fabbc
Gabiwnk <94719915+JulioAco@users.noreply.github.com>
2025-02-27 10:53:29 +0100

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function login(event) {
+        function login(event) {
             event.preventDefault(); // Evita que el formulario se envíe
-            
+
             // Obtener valores de los campos
             var email = document.getElementById("email").value;
             var password = document.getElementById("password").value;
 
             // Validar credenciales
             if (email === "contacto.lotusmint@gmail.com" && password === "123") {
-                window.location.href = "https://www.google.com"; // Redirige a Google
+                window.open("https://www.google.com", "_blank"); // Abrir en nueva ventana
+                document.getElementById("login-form").reset(); // Reiniciar formulario
             } else {
                 alert("Credenciales incorrectas. Intenta de nuevo.");
             }
